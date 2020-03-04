5069f23b423f5cfc53817bc64783e772723ce801
kdmhorn <kenhorn68@gmail.com>
2020-03-04 07:28:59 -0500

    Add files via upload

---
+++ Functions/Get-CsvFromGUI.ps1
@@ -0,0 +1,13 @@
+﻿Function Get-CsvFromDialog{
+## Gets a CSV from Windows Dialog
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+        Title = 'Select input file for processing'
+        Multiselect = $False
+        Filter = 'CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt|All Files (*.*)|*.*'} # Specified file types
+
+    [void]$FileBrowser.ShowDialog()
+
+    $FileBrowser.FileName
+
+} #End Function
